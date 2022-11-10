7eead9d2a59a02ac25a272388e721c32179784e2
Andrei Varfolomeev <drucha5@yandex.ry>
2022-11-11 00:13:56 +0300

    Initial commit :wq

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ git init --инициализация проекта
 git config --global user.mane 'andrei v' --установка имени локально
 git config --global user.email 'drucha5@yandex.ry'
 git add . -- добавление всех фалов
-
+git commit ---отправка файла в репозиторий
+        Initial commit
+        :wq
+git branch slave -- создание новой ветки
 
 '''
