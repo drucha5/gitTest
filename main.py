1407e98a11af7b60bb6367de41f956aa148e1f55
Andrei Varfolomeev <drucha5@yandex.ry>
2022-11-11 00:03:32 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+$ brew install git --- установка git
+git init --инициализация проекта
+git config --global user.mane 'andrei v' --установка имени локально
+git config --global user.email 'drucha5@yandex.ry'
+git add . -- добавление всех фалов
+
+
+'''
